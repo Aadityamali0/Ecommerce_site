46f7933fca63f803049cc5d25833671e49ab20d6
Aadityamali0 <maliaditya275@gmail.com>
2026-07-09 17:32:35 +0545

    product and category fetch

--- store/models.py
+++ store/models.py
@@ -11,7 +11,7 @@ class Product(models.Model):
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete = models.CASCADE, null = True, blank = True)
     description = models.CharField(max_length=300, default='')
-    image = models.ImageField(upload_to = "store/product", default="")
+    image = models.ImageField(upload_to = "image", default="")
     
     def __str__(self):
         return self.product_name
--- store/views.py
+++ store/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Product
+from . models import Product, Category
 
 def home(request):
     products = Product.objects.all()
+    category = Category.objects.all()
     params = {
-        'product': products
+        'product': products,
+        'category' : category
     }
+    print(category)
     return render(request, 'store/home.html', params)
