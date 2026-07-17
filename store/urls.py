071f5c5e117ea8c1caebd1484bcb65eb48548262
Aadityamali0 <maliaditya275@gmail.com>
2026-07-17 16:36:08 +0545

    product-details-view-page-building

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("signup/", views.signup, name="signup")
+    # path("signup/", views.signup, name="signup"),
+    path("product_details/", views.product_details, name="product_details")
 ]
--- store/views.py
+++ store/views.py
@@ -19,27 +19,30 @@ def home(request):
 
     return render(request, 'store/home.html', params)
 
-def signup(request):
-    if request.method == 'GET':
-        return render(request, 'store/signup.html')
-    else:
-        postData = request.POST
-        name = postData.get('name')
-        phone = postData.get('phone')
-        # print(name, email)
-        customer = Customer(name=name,
-                            phone=phone)
-        error_message = None
-        if not name:
-            error_message = "Name is required"
-        elif not phone:
-            error_message = "phone is required"
-        elif len(phone)<10:
-            error_message = "phone number must have 10 numbers"
+# def signup(request):
+#     if request.method == 'GET':
+#         return render(request, 'store/signup.html')
+#     else:
+#         postData = request.POST
+#         name = postData.get('name')
+#         phone = postData.get('phone')
+#         # print(name, email)
+#         customer = Customer(name=name,
+#                             phone=phone)
+#         error_message = None
+#         if not name:
+#             error_message = "Name is required"
+#         elif not phone:
+#             error_message = "phone is required"
+#         elif len(phone)<10:
+#             error_message = "phone number must have 10 numbers"
         
-        if not error_message:
-            messages.success(request, 'signup successful')
-            customer.register()
-            return HttpResponse("Signup Successfull")
-        else:
-            return render(request, 'store/signup.html', {'error': error_message})
+#         if not error_message:
+#             messages.success(request, 'signup successful')
+#             customer.register()
+#             return HttpResponse("Signup Successfull")
+#         else:
+#             return render(request, 'store/signup.html', {'error': error_message})
+
+def product_details(request):
+    return render(request, 'store/product_details.html')
